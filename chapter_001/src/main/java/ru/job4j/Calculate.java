b47a2bcb58cdc756ede44333da4d269d4b4aef2b
dmitrykachala <kosas@mail.ru>
2017-04-12 20:45:28 +0300

    JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,13 +1,15 @@
 package ru.job4j;
 /**
- *Clas Calculate решение задачи части 001 урок 1.
+ *Class Calculate решение задачи части 001 урок 1.
  *author kachala
  *since 12.04.2017
+ *version 1.1
 */
 public class Calculate {
 	
 	/**
 	*Метод выводящий "Hello World." на экран
+	*@param args - arguments from command line
 	*/
 	
 	public static void main (String[] args){
