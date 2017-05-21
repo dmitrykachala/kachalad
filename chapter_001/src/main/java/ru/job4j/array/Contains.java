c5952452633a23e604cf4dd15e49c2e5e63ddd40
dmitrykachala <kosas@mail.ru>
2017-05-21 22:05:43 +0300

    contains

---
+++ chapter_001/src/main/java/ru/job4j/array/Contains.java
@@ -0,0 +1,52 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+/**
+ *Class Contains - класс для определения содержится ли заданная подстрока в указанной строке.
+ *author kachala
+ *since 21.05.2017
+ *version 1.0
+*/
+public class Contains {
+	/**
+	*Метод, который определяет содержится ли заданная подстрока в указанной строке.
+	*@param origin - исходная строка
+	*@param sub - подстрока
+	*@return - результат
+	*/
+	public char[] intoArray (String input) {
+		char[] arr = new char[input.length()];
+		for (int i = 0; i < input.length(); i++) {
+			arr[i] = input.charAt(i);
+		}
+		return arr;
+	}
+	/**
+	*Метод, который определяет содержится ли заданная подстрока в указанной строке.
+	*@param origin - исходная строка
+	*@param sub - подстрока
+	*@return - результат
+	*/
+	public boolean contains(String origin, String sub) {
+		char[] originArr = Contains.intoArray(origin);
+		char[] subArr = Contains.intoArray(sub);
+		if(subArr.length <= originArr.length) {
+				for (int j = 0; j < originArr.length; j++){
+					if((originArr[j] == subArr[0])&&(subArr.length <= originArr.length - j)){
+						char[] tmpArr = Arrays.copyOfRange(originArr, j, originArr.length - 1);
+						int signal = 1;
+						for (int k = 1; k < subArr.length; k++) {
+							if(tmpArr[k] == subArr[k]) {
+								signal += 1;
+							}
+						}
+						if(signal == subArr.length)
+							return true;
+					}
+				}		
+		} else if(i == subArr.length - 1)
+			return true;
+		else
+			return false;
+}
+}
